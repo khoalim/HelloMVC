dfadc8dd01a0921c7dcba2300ca67aa5ce04ee79
Kim Hoalim <kthoalim@gmail.com>
2019-10-01 13:57:11 -0400

    Added Controller and Test Setup

---
+++ HelloMVC.Tests/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace HelloMVC.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
