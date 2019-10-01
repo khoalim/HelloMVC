476386ec3743a1ac87bd649a3ec45d7227d382ef
Kim Hoalim <kthoalim@gmail.com>
2019-10-01 15:08:13 -0400

    Added Greet Controller, Index and Pass Test

---
+++ HelloMVC.Tests/GreetControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit; // this using statement must be added
+using HelloMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Tests
+{
+    public class GreetControllerTests
+    {
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var controller = new GreetController();
+            var result = controller.Index("ThisIsAString");
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ HelloMVC/Controllers/GreetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class GreetController : Controller
+    {
+        public ViewResult Index(string name)
+        {
+            return View();
+        }
+    }
+}
